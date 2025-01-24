04d2543520cd971e022d2a41d20bd1fc29ce631a
abdannsykr <91769581+abdannsykr@users.noreply.github.com>
2025-01-24 18:08:55 +0700

    Project Based Internship

--- KF_Analysis.sql
+++ KF_Analysis.sql
@@ -1,11 +1,13 @@
 SELECT
   t.customer_name,
   p.product_id,
-  p.product_name,  -- Nama obat
-  p.price AS actual_price,  -- Harga obat
+  p.product_name,  
+  p.price AS actual_price,  
   t.discount_percentage,
+
   -- Menghitung harga setelah diskon (nett_sales)
   p.price - (p.price * t.discount_percentage / 100) AS nett_sales,
+
   -- Menentukan persentase laba berdasarkan harga produk (persentase_gross_laba)
   CASE
     WHEN p.price <= 50000 THEN 10
@@ -14,6 +16,7 @@ SELECT
     WHEN p.price > 300000 AND p.price <= 500000 THEN 25
     ELSE 30
   END AS persentase_gross_laba,
+
   -- Menghitung keuntungan bersih (nett_profit)
   (p.price - (p.price * t.discount_percentage / 100)) *
   CASE
@@ -23,17 +26,18 @@ SELECT
     WHEN p.price > 300000 AND p.price <= 500000 THEN 0.25
     ELSE 0.3
   END AS nett_profit,
-  kc.rating AS rating_cabang,  -- Penilaian konsumen terhadap cabang Kimia Farma
-  t.rating AS rating_transaksi,  -- Penilaian konsumen terhadap transaksi
-  t.date,  -- Tanggal transaksi
-  kc.kota,  -- Kota lokasi cabang
-  kc.provinsi,  -- Provinsi lokasi cabang
-  kc.branch_name, -- Nama cabang
+
+  kc.rating AS rating_cabang, 
+  t.rating AS rating_transaksi,  
+  t.date,  
+  kc.kota,  
+  kc.provinsi,  
+  kc.branch_name, 
   t.transaction_id,
-  COUNT(t.transaction_id) OVER (PARTITION BY t.customer_name) AS total_transaction -- Jumlah total transaksi per customer
+  COUNT(t.transaction_id) OVER (PARTITION BY t.customer_name) AS total_transaction 
 FROM
-  `crypto-minutia-447907-d6.rakamin.final_transaction` t  -- Tabel transaksi
+  `crypto-minutia-447907-d6.rakamin.final_transaction` t 
 JOIN
-  `crypto-minutia-447907-d6.rakamin.product` p ON t.product_id = p.product_id  -- Tabel produk
+  `crypto-minutia-447907-d6.rakamin.product` p ON t.product_id = p.product_id 
 JOIN
-  `crypto-minutia-447907-d6.rakamin.kantor_cabang` kc ON t.branch_id = kc.branch_id;  -- Tabel kantor cabang
+  `crypto-minutia-447907-d6.rakamin.kantor_cabang` kc ON t.branch_id = kc.branch_id; 
