a33558bed8dc5d9a0efd19f5a1bb40fa733edd45
abdannsykr <91769581+abdannsykr@users.noreply.github.com>
2025-01-24 16:52:59 +0700

    Project Based Internship

---
+++ KF_Analysis.sql
@@ -0,0 +1,39 @@
+SELECT
+  t.customer_name,  -- Nama customer yang melakukan transaksi
+  p.product_id,
+  p.product_name,  -- Nama obat
+  p.price AS actual_price,  -- Harga obat
+  t.discount_percentage,
+  -- Menghitung harga setelah diskon (nett_sales)
+  p.price - (p.price * t.discount_percentage / 100) AS nett_sales,
+  -- Menentukan persentase laba berdasarkan harga produk (persentase_gross_laba)
+  CASE
+    WHEN p.price <= 50000 THEN 10
+    WHEN p.price > 50000 AND p.price <= 100000 THEN 15
+    WHEN p.price > 100000 AND p.price <= 300000 THEN 20
+    WHEN p.price > 300000 AND p.price <= 500000 THEN 25
+    ELSE 30
+  END AS persentase_gross_laba,
+  -- Menghitung keuntungan bersih (nett_profit)
+  (p.price - (p.price * t.discount_percentage / 100)) *
+  CASE
+    WHEN p.price <= 50000 THEN 0.1
+    WHEN p.price > 50000 AND p.price <= 100000 THEN 0.15
+    WHEN p.price > 100000 AND p.price <= 300000 THEN 0.2
+    WHEN p.price > 300000 AND p.price <= 500000 THEN 0.25
+    ELSE 0.3
+  END AS nett_profit,
+  kc.rating AS rating_cabang,  -- Penilaian konsumen terhadap cabang Kimia Farma
+  t.rating AS rating_transaksi,  -- Penilaian konsumen terhadap transaksi
+  t.date,  -- Tanggal transaksi
+  kc.kota,  -- Kota lokasi cabang
+  kc.provinsi,  -- Provinsi lokasi cabang
+  kc.branch_name, -- Nama cabang
+  t.transaction_id,
+  COUNT(t.transaction_id) OVER (PARTITION BY t.customer_name) AS total_transaction -- Jumlah total transaksi per customer
+FROM
+  `crypto-minutia-447907-d6.rakamin.final_transaction` t  -- Tabel transaksi
+JOIN
+  `crypto-minutia-447907-d6.rakamin.product` p ON t.product_id = p.product_id  -- Tabel produk
+JOIN
+  `crypto-minutia-447907-d6.rakamin.kantor_cabang` kc ON t.branch_id = kc.branch_id;  -- Tabel kantor cabang
--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# Kimia-Farma-Big-Data-Analytics
- 
+# Kimia Farma Business Performance Analysis
+
+## Deskripsi Proyek
+
+Kimia Farma, sebagai salah satu perusahaan farmasi terbesar di Indonesia, memerlukan analisis mendalam terhadap kinerja bisnis selama periode 2020-2023. Data besar yang mencakup transaksi, stok produk, serta penilaian cabang dan pelanggan dikelola menggunakan **Google BigQuery** untuk memberikan wawasan strategis dan mendukung pengambilan keputusan berbasis data.
+
+Proyek ini bertujuan untuk menciptakan **tabel analisis** yang mencakup metrik utama, seperti:
+- **Pendapatan Bersih** (nett sales)
+- **Persentase Laba** (gross margin)
+- **Keuntungan Bersih** (nett profit)
+
+### Dashboard
+
+Berikut adalah visualisasi **Dashboard Kimia Farma** yang memberikan gambaran mengenai kinerja bisnis selama periode 2020-2023:
+
+![Dashboard Kimia Farma](images/Dashboard_Project_Based_Internship.png)
+
+### Tabel Relasional
+
+Berikut adalah gambar dari tabel relasional yang digunakan dalam analisis data:
+
+![Tabel Relasional](images/table_relational.png)
